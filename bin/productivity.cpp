7cb35f9d753e808331c720f6d2e6e0bce58f99e4
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-19 18:59:45 +0200

    100/100

--- bin/productivity.cpp
+++ bin/productivity.cpp
@@ -1,42 +1,20 @@
-/**
- * @file productivity.cpp
- *
- * @version 01.01 2021519
- *
- * @brief https://training.olinfo.it/#/task/ois_productivity/statement
- *
- * @ingroup productivity
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
- */
-
-// Includes
 #include <bits/stdc++.h>
 using namespace std;
 
-// Variabiles
-int N;
+#define DEBUG
 
-// Main code
-int main()
-{
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+int N, M, E, sol;
 
-  // Input
-  cin >> N;
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-  // Code
-  // ...
+    cin >> N >> M >> E;
+    sol = 0;
 
-  // Output
-  cout << N << endl;
+    for (size_t i = M; i <= N; ++i)
+        if (E % i > sol)
+            sol = E % i;
 
-  // End
-  return 0;
+    cout << sol << endl;
 }
